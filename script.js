15272524bacffaf4f9a916b302c97746a0fbf6fd
Haqq Bassit <munagahmnh@gmail.com>
2024-01-02 20:19:26 +0000

    Add Grid Resizing Functionality

--- script.js
+++ script.js
@@ -1,19 +1,50 @@
 const container = document.querySelector(".container");
 
-for (let i = 0; i < 16*16; i++) {
-    const squareDiv = document.createElement('div');
-    squareDiv.classList.add('grid-square');
-    squareDiv.style.padding = 0;
-    squareDiv.style.width = '30px';
-    squareDiv.style.height = '30px';
-    squareDiv.style.border = '2px solid grey';
-    container.appendChild(squareDiv);
+const INITIAL_SIZE = 16;
+createGrid(INITIAL_SIZE);
+
+const btn = document.querySelector(".btn");
+
+btn.addEventListener('click', resizeGrid);
+
+
+function createGrid(size) {
+    for (let i = 0; i < size*size; i++) {
+        const squareDiv = document.createElement('div');
+        squareDiv.classList.add('grid-square');
+        squareDiv.style.padding = 0;
+        squareDiv.style.boxSizing = "border-box";
+        squareDiv.style.border = '0.5px solid grey';
+        squareDiv.style.flex =  "1 1 calc(100% / " + size + ")";
+        
+        container.appendChild(squareDiv);
+    }
+
+    let gridSquares = document.querySelectorAll(".grid-square");
+
+    gridSquares.forEach(gridSquare => {
+        gridSquare.addEventListener('mouseover', () => {
+            gridSquare.style.backgroundColor = 'black';
+        });
+    });
 }
 
-const gridSquares = document.querySelectorAll(".grid-square");
+function resizeGrid() {
+    let newSize = prompt("Enter grid size(1-80)");
+    if (newSize === null) return;
+    newSize = parseInt(newSize);
 
-gridSquares.forEach(gridSquare => {
-    gridSquare.addEventListener('mouseover', () => {
-        gridSquare.style.backgroundColor = 'black';
-    });
-});
+    while (isNaN(newSize) || newSize < 1 || newSize > 80) {
+        if (isNaN(newSize)) newSize = prompt("Invalid input! (Enter integer)\nEnter grid size");
+        else newSize = prompt("Invalid Input!\nEnter Integer within range")
+        
+
+        if (newSize === null) return;
+
+        newSize = parseInt(newSize);
+    }
+
+    while (container.firstChild) container.removeChild(container.firstChild);
+
+    createGrid(newSize);
+}
